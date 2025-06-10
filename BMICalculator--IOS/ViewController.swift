645dd8b9d8acef5d95d59a3e4587287dcbaffd13
Bee1002 <alvaroslim@gmail.com>
2025-06-10 19:53:42 +0200

    agegados pesos

--- BMICalculator--IOS/ViewController.swift
+++ BMICalculator--IOS/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     
-       @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    /*@IBOutlet weak var shareResult: UIButton!*/
     
-    var weight: Float = 0
-    var height: Float = 0
+    var weight: Float = 60
+    var height: Float = 150
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,20 +37,61 @@ class ViewController: UIViewController {
         resultLabel.text = "\(String(format: "%.2f", bmi))"
         
         // Determinar el estado según el IMC
-        if bmi < 18.5 {
-            statusLabel.text = "Delgado"
-            statusLabel.textColor = UIColor.blue
-        } else if bmi >= 18.5 && bmi < 25 {
+        /* if bmi < 18.5 {
+         statusLabel.text = "Delgado"
+         statusLabel.textColor = UIColor.blue
+         } else if bmi >= 18.5 && bmi < 25 {
+         statusLabel.text = "Normal"
+         statusLabel.textColor = UIColor.green
+         } else {
+         statusLabel.text = "Obeso"
+         statusLabel.textColor = UIColor.red
+         }*/
+        
+        // Otra manera de hacerlo con
+        
+        var bmiColor = UIColor.black
+        var showAlert = false
+        
+        switch bmi {
+        case ..<18.5:
+            statusLabel.text = "Bajopeso"
+            bmiColor = UIColor(named: "bmi-bajopeso")!
+        case 18.5..<25:
             statusLabel.text = "Normal"
-            statusLabel.textColor = UIColor.green
-        } else {
+            bmiColor = UIColor(named: "bmi-peso-normal")!
+        case 25..<30:
+            statusLabel.text = "Sobrepeso"
+            bmiColor = UIColor(named: "bmi-sobrepeso")!
+        case 30..<35:
             statusLabel.text = "Obeso"
-            statusLabel.textColor = UIColor.red
+            bmiColor = UIColor(named: "bmi-obeso")!
+            showAlert = true
+        default:
+            statusLabel.text = "Obesidad Extrema"
+            bmiColor = UIColor(named: "bmi-obesidad-extrema")!
+            showAlert = true
         }
+        
+        resultLabel.textColor = bmiColor
+        
+        if showAlert {
+            let alert = UIAlertController(
+                title: "Riesgo de salud",
+                message: "Tu peso actual pone en grave riesgo tu vida. Busca ayuda medica cuanto antes.",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Mas informacion", style: .default, handler:
+                                            { action in
+                // He pulsado mas informacion
+                let adviceSiteURL = "https://www.who.int/es/news-room/fact-sheets/detail/obesity-and-overweight"
+                if let url = URL(string: adviceSiteURL),UIApplication.shared.canOpenURL(url) {
+                    UIApplication.shared.open(url)
+                }
+            } ))
+            alert.addAction(UIAlertAction(title: "Cerrar", style: .destructive, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             
+        }
         
     }
-   
-      
 }
-
